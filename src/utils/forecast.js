739c1123bbca6ecd25f1acda22881428af96ed8f
Aman Khalfay <amankhalfay98@gmail.com>
2020-07-28 10:34:00 +0530

    Add UV Index and Windspeed to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -10,7 +10,7 @@ const forecast = (latitude, longitude, callback) => {
         } else if (body.error) {
             callback('Unable to find location', undefined)
         } else {
-            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. ')
+            callback(undefined, body.current.weather_descriptions[0] + '. It is currently ' + body.current.temperature + ' degrees out. It feels like ' + body.current.feelslike + ' degrees out. The UV Index is  ' + body.current.uv_index + ', with a Wind Speed of ' + body.current.wind_speed)
         }
     })
 }
